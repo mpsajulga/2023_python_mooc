72b7f6f0155583b4be3b2c36b465378baea230a0
mpsajulga <mychalphillipsajulga@gmail.com>
2023-01-13 14:38:56 +0800

    Learned how to use the f-string.

---
+++ ex_10.py
@@ -0,0 +1,18 @@
+name = "Tim Tester"
+age = 20
+skill_1 = "python"
+level_1 = "beginner"
+skill_2 = "java"
+level_2 = "veteran"
+skill_3 = "programming"
+level_3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print(f"my name is {name}, I am {age} years old \n")
+print("my skills are")
+print(f" - {skill_1} ({level_1})")
+print(f" - {skill_2} ({level_2})")
+print(f" - {skill_3} ({level_3}) \n")
+
+print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
