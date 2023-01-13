8fe6f3abdfd5874055e0a3948093328d263e71dd
mpsajulga <mychalphillipsajulga@gmail.com>
2023-01-13 12:27:43 +0800

    Learning mastery of multiple input strings coupled with exclamation points and dashes

---
+++ ex_3.py
@@ -0,0 +1,15 @@
+"""
+
+Here is a program which should ask for three utterances and print them out, like so:
+
+The 1st part: hickory
+The 2nd part: dickory
+The 3rd part: dock
+hickory-dickory-dock!
+
+"""
+
+first_part = input("The 1st part: ")
+second_part = input("The 2nd part: ")
+third_part = input("The 3rd part: ")
+print((first_part)+"-"+(second_part)+"-"+(third_part)"!")
