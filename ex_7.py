d105f6f8bde6e6424fb0881001455b9efe3b6aae
mpsajulga <mychalphillipsajulga@gmail.com>
2023-01-13 13:40:20 +0800

    Learned how to compute minutes in a year.

---
+++ ex_7.py
@@ -0,0 +1,6 @@
+"""
+Please write a program which prints out the number of minutes in a year. Use Python code to perform the calculation, as in the previous code example.
+"""
+
+print("Minutes in a year:")
+print(365*24*60) #365 days, 24 hours in each day, 60 minutes in an hour
