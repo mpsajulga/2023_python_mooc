7134bda5b6b9f03c7c0f55b19658cbe3f1fc2005
mpsajulga <mychalphillipsajulga@gmail.com>
2023-01-13 14:49:22 +0800

    Learned how to +-*/ two integer values.

---
+++ ex_11.py
@@ -0,0 +1,12 @@
+x = 27
+y = 15
+
+add = x+y
+minus = x-y
+multiply = x*y
+divide = x/y
+
+print(f"{x} + {y} = {add}")
+print(f"{x} - {y} = {minus}")
+print(f"{x} * {y} = {multiply}")
+print(f"{x} / {y} = {divide}")
