80b6b927a6b5984b38ae40c85e946482643c1a94
mpsajulga <mychalphillipsajulga@gmail.com>
2023-01-14 10:06:39 +0800

    Answered the Absolute Value

---
+++ part_2/ex_2.py
@@ -0,0 +1,7 @@
+in_number = int(input("Please type in a number: "))
+
+if in_number < 0:
+    absolute_value_of_number = in_number * -1
+    print(f"The absolute value of this number is {absolute_value_of_number}")
+else:
+    print(f"The absolut value of this number is {in_number}")
