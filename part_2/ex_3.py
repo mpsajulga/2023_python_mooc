f04f668d4bfea0845de64f79a477f065596e5442
mpsajulga <mychalphillipsajulga@gmail.com>
2023-01-17 09:18:56 +0800

    Answered Typecasting

---
+++ part_2/ex_3.py
@@ -0,0 +1,5 @@
+input_number = float(input("Please type in a number: "))
+integer_number = int(input_number)
+print(f"Integer part: {integer_number}")
+decimal_number = input_number - integer_number
+print(f"Decimal part: {decimal_number}")
