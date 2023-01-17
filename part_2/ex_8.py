62f5f8fb6b5e51c5a55d11b7e2b07e98451f769c
mpsajulga <mychalphillipsajulga@gmail.com>
2023-01-17 18:39:38 +0800

    Answered the 'Nephews exercise'

--- part_2/ex_8.py
+++ part_2/ex_8.py
@@ -2,7 +2,7 @@ input_age = int(input("What is your age? "))
 
 if input_age >= 5:
     print(f"Ok, you're {input_age} years old")
-if input_age > 0 and input_age <= 4:
+if input_age >= 0 and input_age <= 4:
     print("I suspect you can't write quite yet...")
-if input_age <= 0:
+if input_age < 0:
     print("That must be a mistake")
---
+++ part_2/ex_9.py
@@ -0,0 +1,9 @@
+input_name = str(input("Please type in a name: "))
+
+if input_name == "Huey" or input_name == "Dewey" or input_name == "Louie":
+    print("I think you might be one of Donald Duck's nephews.")
+elif input_name == "Morty" or input_name == "Ferdie":
+    print("I think you might be one of Mickey Mouse's nephews.")
+else:
+    print("You're not a nephew of any character I know of.")
+    
