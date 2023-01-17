3b06c31556c8bda66d47171fcff59f946ce98ba7
mpsajulga <mychalphillipsajulga@gmail.com>
2023-01-17 10:01:21 +0800

    Answered Greater than or equal to

---
+++ part_2/ex_5.py
@@ -0,0 +1,9 @@
+input_int_number = int(input("Please type in the first number: "))
+input_int_number_2 = int(input("Please type in another number: "))
+
+if input_int_number > input_int_number_2:
+    print(f"The greater number was: {input_int_number}")
+if input_int_number < input_int_number_2:
+    print(f"The greater number was: {input_int_number_2}")
+if input_int_number_2 == input_int_number:
+    print("The numbers are equal!")
