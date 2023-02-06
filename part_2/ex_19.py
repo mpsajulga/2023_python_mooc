3dfc5c46e14ceb703b4429cc2b75aa5469dff6c6
mpsajulga <mychalphillipsajulga@gmail.com>
2023-02-06 19:46:56 +0800

    Answered 'Pin and Number Attempts'

--- part_2/ex_19.py
+++ part_2/ex_19.py
@@ -1,31 +1,10 @@
-""""
-attempts = 0
-master_code_pin = 4321
-input_code_pin = ""
-
-while True:
-    input_code_pin = input("PIN: ")
-    attempts += 1
-    
-    if input_code_pin != "4321":
-        print("Wrong")
-    #elif input_code_pin == master_code_pin and attempts == 1:
-       # break
-        #print("Corect! It took you one single attempt!")
-    elif input_code_pin == master_code_pin:
-        break
-        print(f"Corect! It took you {attempts} attempts")
-""""
-"""
-pins = ""
 attempts = 0
 master_pin = 4321
 
 while True:
     pin = input("PIN: ")
     attempts += 1
-    pins += pin + ", "
-    
+        
     if pin == "4321":
         success = True
         break
@@ -35,8 +14,6 @@ while True:
 if success:
     if attempts == 1:
         print(f"Correct! It only took you one single attempt!")
-        print(f"{pins}")
     else:
         print(f"Correct! It took you {attempts} attempts")
-        print(f"{pins}")
-"""
+    
