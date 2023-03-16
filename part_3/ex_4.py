c597ee8c6c92dca0ce841809d96ab7204944480d
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-16 20:13:55 +0800

    Answered 'Powers of Two' exercise

---
+++ part_3/ex_4.py
@@ -0,0 +1,17 @@
+'''
+Please write a program which asks the user to type in an upper limit. 
+The program then prints out numbers so that each subsequent number is the previous one doubled, starting from the number 1. 
+That is, the program prints out powers of two in order.
+
+The execution of the program finishes when the next number to be printed would be greater than the limit set by the user. 
+No numbers greater than the limit should be printed.
+'''
+
+upper_limit = int(input("Upper limit: "))
+
+numbers = 1
+
+while numbers <= upper_limit:
+    print(numbers)
+    numbers *= 2   
+    
