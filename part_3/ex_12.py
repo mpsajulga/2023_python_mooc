69e20d1c4f37bf60f960f108604309c56219d4b3
mpsajulga <mychalphillipsajulga@gmail.com>
2023-03-19 21:12:38 +0800

    Answered ex#12 'A line of hashes'

---
+++ part_3/ex_12.py
@@ -0,0 +1,7 @@
+'''Please write a program which prints out a line of hash characters, 
+the width of which is chosen by the user.'''
+
+input_string = int(input("Width: "))
+hash = "#"
+
+print(hash*input_string)
