3e661da8e1e8c1bfd542980fc46fcd73c248e25d
mpsajulga <mychalphillipsajulga@gmail.com>
2023-04-21 11:36:57 +0800

    Answered ex_29. Learned how to use the def function.

---
+++ part_3/ex_29.py
@@ -0,0 +1,27 @@
+'''Please write a function named seven_brothers. 
+When the function is called, it should print out the names of the seven brothers in alphabetical order, 
+as in the example below. See the similarly named exercise in part 1 for more details about the brothers.
+
+Sample output
+Aapo
+Eero
+Juhani
+Lauri
+Simeoni
+Timo
+Tuomas'''
+
+
+def seven_brothers():
+    print("Aapo")
+    print("Eero")
+    print("Juhani")
+    print("Lauri")
+    print("Simeoni")
+    print("Timo")
+    print("Tuomas")
+
+
+if __name__ == "__main__":
+
+    seven_brothers()
